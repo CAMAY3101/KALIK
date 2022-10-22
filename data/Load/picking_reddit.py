4fa48b217e195c8a97690e49ab55a525c2c2d43e
Maximiliano Martinez Marquez <a01251527@itesm.mx>
2022-10-22 16:07:26 -0500

    Update picking_reddit.py
    Divided main function into multiple files for better readability

--- data/Load/picking_reddit.py
+++ data/Load/picking_reddit.py
@@ -1,41 +1,51 @@
+from ast import Num
 import pandas as pd
 import numpy as np
 import datetime
 from pmaw import PushshiftAPI
 
 def main():
-    # get epoch of date
+    subreddits = ['ethereum']# ,'ethtrader', 'ethfinance' 'AllCryptoBets', 'CryptoCurrency', 'Crypto_Currency_News', 'CryptoCurrencies', 'CryptoMarkets']
+    df = search(subreddits)
+    creat_csv(df)
+
+    
+def sub_filter(item):
+    return item['score'] > 100 #and item['num_comments'] > 100
+
+
+def search(subreddits):
+    api = PushshiftAPI(num_workers=40,)
+    subreddits_posts = dict()
     after_time = int(datetime.datetime(2019, 10, 1, 0, 0).timestamp())
     before_time = int(datetime.datetime(2022, 9, 30, 0, 0).timestamp())
-
-    api = PushshiftAPI()
-
-    subreddits = ['ethereum' ,'ethtrader', 'ethfinance' 'AllCryptoBets', 'CryptoCurrency', 'Crypto_Currency_News', 'CryptoCurrencies', 'CryptoMarkets']
-    subreddits_posts = dict()
 
     print("-------------start-------------")
     for subreddit in subreddits:
         print("Beginning search for", subreddit)
         subreddits_posts[subreddit] = api.search_submissions(subreddit=subreddit,
-                                            filter=['title','selftext','created_utc','subreddit'],
+                                            filter=['title','selftext','created_utc','subreddit','score','full_link','num_comments'],
+                                            filter_fn=sub_filter,
                                             after=after_time,
                                             before=before_time,                 
-                                            #limit=10000,
+                                            #limit=1000,
+                                            #limit=1000,
                                             )
         print("Ended search for", subreddit)
     print("--------------end--------------")
-
-
+    # join for multiple searches
     df = pd.DataFrame()
     for subreddit in subreddits:
         df = pd.concat([df, pd.DataFrame([post for post in subreddits_posts[subreddit]])])
-        
 
-    # It is necessary to split df as it is too big.
-    result = np.array_split(df, 2)
+    return df
     
-    result[0].to_csv(r'C:\Users\SR118\Desktop\Reddit_1.csv', index = False, header=True)
-    result[1].to_csv(r'C:\Users\SR118\Desktop\Reddit_2.csv', index = False, header=True)
+
+def creat_csv(df):
+    result = np.array_split(df, 2)
+    result[0].to_csv(r'C:\Users\SR118\Desktop\Reddit_1_t.csv', index = False, header=True)
+    result[1].to_csv(r'C:\Users\SR118\Desktop\Reddit_2_t.csv', index = False, header=True)
+
 
 if __name__ == "__main__":
     main()
