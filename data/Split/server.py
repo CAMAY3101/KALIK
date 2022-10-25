bde369ba8dc16127795e5fa347f02aeecd319690
Aylin <A01379272@itesm.mx>
2022-10-24 23:10:57 -0500

    dataset exploration

--- data/Split/server.py
+++ data/Split/server.py
@@ -1,4 +1,12 @@
 from pymongo import MongoClient
-client = MongoClient(
-    "mongodb+srv://kalikTeam:NietosDeGauss@cluster0.uhbfdme.mongodb.net/?retryWrites=true&w=majority")
-db = client.test
+import pandas as pd
+#import os
+
+#entries = os.listdir('../Load/datasets/')
+#print(entries)
+df_Binance = pd.read_csv('../Load/datasets/Binance.csv')
+print(df_Binance.columns)
+df_eth = pd.read_csv('../Load/datasets/ETH-USD.csv')
+print(df_eth.columns)
+df_reddit = pd.read_csv('../Load/datasets/Reddit_filtered/Reddit_1.csv')
+print(df_reddit.columns)
