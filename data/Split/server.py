93dfb365114af8cde80fd55e0a360868330be4b9
Aylin <A01379272@itesm.mx>
2022-10-25 16:49:45 -0500

    Split eth dataset

--- data/Split/server.py
+++ data/Split/server.py
@@ -1,12 +1,22 @@
 from pymongo import MongoClient
 import pandas as pd
-#import os
+import os
 
-#entries = os.listdir('../Load/datasets/')
-#print(entries)
+#-------------------FUNCTIONS-------------------#
+
+
+
+#-------------------CHECK VARIABLES-------------------#
 df_Binance = pd.read_csv('../Load/datasets/Binance.csv')
-print(df_Binance.columns)
+print("-------------------Binance-------------------")
+print(df_Binance.head(5))
 df_eth = pd.read_csv('../Load/datasets/ETH-USD.csv')
-print(df_eth.columns)
+print("-------------------ETH-USD-------------------")
+print(df_eth.head(5))
 df_reddit = pd.read_csv('../Load/datasets/Reddit_filtered/Reddit_1.csv')
-print(df_reddit.columns)
+print("-------------------Reddit-------------------")   
+print(df_reddit.head(5))
+#-------------------FIX VARIABLES-------------------#
+df_eth = df_eth['date;adj_close;volume'].str.split(';', expand=True) #split dataset
+df_eth.columns = ['date', 'adj_close', 'volume'] #rename columns
+print(df_eth.head(5))
