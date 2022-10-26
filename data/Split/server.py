2a300b4c7f54bccfdd776bf7d53715200e67d216
Aylin <A01379272@itesm.mx>
2022-10-26 13:50:45 -0500

    Final commit

--- data/Split/server.py
+++ data/Split/server.py
@@ -1,27 +1,58 @@
+from ast import Return
 from pymongo import MongoClient
 import pandas as pd
 import os
 
 #-------------------FUNCTIONS-------------------#
+def convert_to_dict(dataframe):
+    return dataframe.to_dict('records')
 
-#-------------------VARIABLES-------------------#
-df_Binance = pd.read_csv('../Load/datasets/Binance.csv')
-# print("-------------------Binance-------------------")
-# print(df_Binance.head(5))
-df_eth = pd.read_csv('../Load/datasets/ETH-USD.csv')
-# print("-------------------ETH-USD-------------------")
-# print(df_eth.head(5))
-df_reddit = pd.read_csv('../Load/datasets/Reddit_filtered/Reddit_1.csv')
-# print("-------------------Reddit-------------------")   
-# print(df_reddit.head(5))
+def fix_dataframes(dataframe):
+    col = dataframe.columns[0]
+    cols_names = col.split(";")
+    dfNew = dataframe[col].str.split(';', expand=True)  # split dataset
+    dfNew.columns = cols_names  # rename columns
+    return dfNew
 
-#-------------------FIX VARIABLES-------------------#
-df_eth = df_eth['date;adj_close;volume'].str.split(';', expand=True) #split dataset
-df_eth.columns = ['date', 'adj_close', 'volume'] #rename columns
-print(df_eth.head(5))
+#-------------------DATAFRAMES-------------------#
+df_ETH_BTC_BINANCE = pd.read_csv('../Load/datasets/Binance/ETH-BTC-klines.csv')
+df_ETH_USDT_BINANCE = pd.read_csv('../Load/datasets/Binance/ETH-USDT-klines.csv')
+df_BNB_ETH_BINANCE = pd.read_csv('../Load/datasets/Binance/BNB-ETH-klines.csv')
+df_XRP_ETH_BINANCE = pd.read_csv('../Load/datasets/Binance/XRP-ETH-klines.csv')
+df_ETH_USD_Yahoo = pd.read_csv('../Load/datasets/Yahoo/ETH-USD.csv')
+
+#-------------------FIX DATAFRAMES-------------------#
+df_ETH_BTC_BINANCE = fix_dataframes(df_ETH_BTC_BINANCE)
+df_ETH_USDT_BINANCE = fix_dataframes(df_ETH_USDT_BINANCE)
+df_BNB_ETH_BINANCE = fix_dataframes(df_BNB_ETH_BINANCE)
+df_XRP_ETH_BINANCE = fix_dataframes(df_XRP_ETH_BINANCE)
+df_ETH_USD_Yahoo = fix_dataframes(df_ETH_USD_Yahoo)
+
+#-------------------CONVERT TO JSON-------------------#
+json_ETH_BTC_BINANCE = convert_to_dict(df_ETH_BTC_BINANCE)
+json_ETH_USDT_BINANCE = convert_to_dict(df_ETH_USDT_BINANCE)
+json_BNB_ETH_BINANCE = convert_to_dict(df_BNB_ETH_BINANCE)
+json_XRP_ETH_BINANCE = convert_to_dict(df_XRP_ETH_BINANCE)
+json_XRP_ETH_BINANCE = convert_to_dict(df_XRP_ETH_BINANCE)
+json_ETH_USD_Yahoo = convert_to_dict(df_ETH_USD_Yahoo)
 
 #-------------------CONECCTION TO MONGODB-------------------#
 client = MongoClient("mongodb+srv://kalikTeam:NietosDeGauss@cluster0.uhbfdme.mongodb.net/?retryWrites=true&w=majority")
 db = client.get_database('input-kalik')
-eth_usd_info = db.ETH_USD_Yahoo
-print(eth_usd_info.count_documents({}))
+
+#-------------------SELECT COLLECTIONS -------------------#
+coll_ETH_BTC_BINANCE = db.ETH_BTC_BINANCE
+coll_ETH_USDT_BINANCE = db.ETH_USDT_BINANCE
+coll_BNB_ETH_BINANCE = db.BNB_ETH_BINANCE
+coll_XRP_ETH_BINANCE = db.XRP_ETH_BINANCE
+coll_ETH_USD_Yahoo = db.ETH_USD_Yahoo
+
+#-------------------INSERT DATA-------------------#
+
+#NOTE: If you want to insert data, you must delete the collection first. Run the following code just once.
+
+coll_ETH_BTC_BINANCE.insert_many(json_ETH_BTC_BINANCE)
+coll_ETH_USDT_BINANCE.insert_many(json_ETH_USDT_BINANCE)
+coll_BNB_ETH_BINANCE.insert_many(json_BNB_ETH_BINANCE)
+coll_XRP_ETH_BINANCE.insert_many(json_XRP_ETH_BINANCE)
+coll_ETH_USD_Yahoo.insert_many(json_ETH_USD_Yahoo)
