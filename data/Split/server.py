584c675725b5923c5577f8a71fb7b560dd10c611
Aylin <A01379272@itesm.mx>
2022-10-25 23:17:50 -0500

    connection to mongoDB

--- data/Split/server.py
+++ data/Split/server.py
@@ -4,19 +4,24 @@ import os
 
 #-------------------FUNCTIONS-------------------#
 
-
-
-#-------------------CHECK VARIABLES-------------------#
+#-------------------VARIABLES-------------------#
 df_Binance = pd.read_csv('../Load/datasets/Binance.csv')
-print("-------------------Binance-------------------")
-print(df_Binance.head(5))
+# print("-------------------Binance-------------------")
+# print(df_Binance.head(5))
 df_eth = pd.read_csv('../Load/datasets/ETH-USD.csv')
-print("-------------------ETH-USD-------------------")
-print(df_eth.head(5))
+# print("-------------------ETH-USD-------------------")
+# print(df_eth.head(5))
 df_reddit = pd.read_csv('../Load/datasets/Reddit_filtered/Reddit_1.csv')
-print("-------------------Reddit-------------------")   
-print(df_reddit.head(5))
+# print("-------------------Reddit-------------------")   
+# print(df_reddit.head(5))
+
 #-------------------FIX VARIABLES-------------------#
 df_eth = df_eth['date;adj_close;volume'].str.split(';', expand=True) #split dataset
 df_eth.columns = ['date', 'adj_close', 'volume'] #rename columns
 print(df_eth.head(5))
+
+#-------------------CONECCTION TO MONGODB-------------------#
+client = MongoClient("mongodb+srv://kalikTeam:NietosDeGauss@cluster0.uhbfdme.mongodb.net/?retryWrites=true&w=majority")
+db = client.get_database('input-kalik')
+eth_usd_info = db.ETH_USD_Yahoo
+print(eth_usd_info.count_documents({}))
