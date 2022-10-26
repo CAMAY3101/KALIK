1fe50c7773b10f7c5fc48176bba4120271d8071d
Maximiliano Martinez Marquez <a01251527@itesm.mx>
2022-10-26 16:54:46 -0500

    Update picking_reddit_day.py

--- data/Load/picking_reddit_day.py
+++ data/Load/picking_reddit_day.py
@@ -13,11 +13,6 @@ import datetime
 # Wrapper for the Pushshift API used to retrieve Reddit submissions
 from pmaw import PushshiftAPI
 
-"""
-Obtain data from various subreddits and generate a csv file.
-How to run file:
-python picking_reddit.py
-"""
 
 def main():
     # List of subreddits used to retrieve submissions
@@ -29,16 +24,12 @@ def main():
         y, m, d = list(map(int, str(day).split(' ')[0].split('-')))
         date_range_list.append(int(datetime.datetime(y, m, d, 0, 0).timestamp()))
     
-    print(len(search('ethereum', date_range_list[0], date_range_list[1])))
-    #df = search(subreddits)
-    #create_csv(df)
-
-
-# Filter for submission search
-def sub_filter(item):
-    return item['score'] > 50 #and item['num_comments'] > 100
-
-
+    num_post_list = []
+    for subreddit in subreddits:
+        for day in date_range_list:
+            num_post_list.append(len(search(subreddit, day-86400, day+86400)))
+    #print(len(search('ethereum', date_range_list[0], date_range_list[1])))
+    
 def search(subreddit, before, after):
     api = PushshiftAPI(num_workers=40,)
 
@@ -47,7 +38,6 @@ def search(subreddit, before, after):
     print("Beginning search for", subreddit)
     s = api.search_submissions(subreddit=subreddit,
             filter=['title','selftext','created_utc','subreddit','score','num_comments'],#,'full_link'],
-            #filter_fn=sub_filter,
             after=after,
             before=before,                                                         
                                             )
@@ -56,15 +46,6 @@ def search(subreddit, before, after):
 
     return pd.DataFrame(s)
     
-
-def create_csv(df):
-    # Split DataFrame
-    result = np.array_split(df, 4)
-    # Generate csv files
-    result[0].to_csv(r'./datasets/Reddit_unfiltered/Reddit_1.csv', index = False, header=True)
-    result[1].to_csv(r'./datasets/Reddit_unfiltered/Reddit_2.csv', index = False, header=True)
-    result[2].to_csv(r'./datasets/Reddit_unfiltered/Reddit_3.csv', index = False, header=True)
-    result[3].to_csv(r'./datasets/Reddit_unfiltered/Reddit_4.csv', index = False, header=True)
 
 
 if __name__ == "__main__":
